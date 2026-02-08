7fa0e0b81e103e69a6e79592eccb4d8d9fea495d
arnob <farhanasfar21@gmail.com>
2026-02-09 02:37:58 +0600

    Add location model

--- property_app/listings/models.py
+++ property_app/listings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from __future__ import annotations
+
+import os
+import uuid
+
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.text import slugify  #slugify is used to create url-friendly slugs from strings.
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=140, unique=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+    
+    # overriding the save() func. 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name) #turns "new york" into /new-york
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
