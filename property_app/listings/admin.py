629b01e65781404d8e350848b45dd73c2c83c08d
farhan <farhanasfar21@gmail.com>
2026-02-09 10:55:58 +0600

    Register Location in admin panel

--- property_app/listings/admin.py
+++ property_app/listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from .models import Location, Property, PropertyImage
 
 # Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ("name",)
