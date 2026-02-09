bbd3d39dc6fa2ba41b276869a63cd0ba796f7e8a
farhan <farhanasfar21@gmail.com>
2026-02-09 11:39:46 +0600

    Register property model in admin panel

--- property_app/listings/admin.py
+++ property_app/listings/admin.py
@@ -16,3 +16,21 @@ class PropertyImageInline(admin.TabularInline):
     extra = 1
     fields = ("image", "is_primary", "alt_text")
     can_delete = True 
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("external_id", "title", "location", "address", "created_at")
+    list_filter = ("location",)
+    search_fields = ("external_id", "title", "address", "location__name")
+    ordering = ("-created_at",)
+
+    inlines = [PropertyImageInline]
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+
+        obj = form.instance
+        primary_count = obj.images.filter(is_primary = True).count()
+
+        if primary_count > 1:
+            return ValidationError("Select only one primary image for a property")
