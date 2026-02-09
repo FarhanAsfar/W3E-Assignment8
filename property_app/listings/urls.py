850daa8c945a00e666c3128936763f9659716a31
farhan <farhanasfar21@gmail.com>
2026-02-09 17:22:02 +0600

    Register urls path for location and properties

--- property_app/listings/urls.py
+++ property_app/listings/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LocationViewSet, PropertyViewSet
+
+
+router = DefaultRouter()
+router.register(r"locations", LocationViewSet, basename="locations")
+router.register(r"properties", PropertyViewSet, basename="properties")
+
 
 urlpatterns = [
-    
+    path("", include(router.urls)),
 ]
