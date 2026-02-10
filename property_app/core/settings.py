7bad6f3a7f91cfbe73a1e2e07ccabd375b8295aa
farhan <farhanasfar21@gmail.com>
2026-02-10 16:05:46 +0600

    Change page size: pagination

--- property_app/core/settings.py
+++ property_app/core/settings.py
@@ -125,6 +125,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 8,
     "EXCEPTION_HANDLER": "listings.exceptions.api_exception_handler",
 }
